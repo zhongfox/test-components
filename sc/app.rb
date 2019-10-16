08fd31f218448aa4bb48a3556244be326ab1a4a8
foxzhong <foxzhong@tencent.com>
2019-10-16 20:26:53 +0800

    sc: server_client

---
+++ sc/app.rb
@@ -0,0 +1,61 @@
+require 'sinatra'
+require 'socket'
+require 'net/http'
+$stdout.sync = true
+
+service = ENV['SERVICE']
+destinations = ENV['DESTINATIONS']
+
+def start_service service
+  puts "starting service #{service}"
+
+  set :bind, '0.0.0.0'
+  set :port, 7000
+  get "/#{service}" do
+    content_type :text
+    puts "=> receiving call from #{request.ip} #{headers["X-Forwarded-For"]}"
+    "<= #{service}(#{Socket.gethostname} #{my_first_private_ipv4}) say hi"
+  end
+end
+
+def call_service service
+  puts "calling service #{service}:"
+  uri = URI("http://#{service}:7000/#{service}")
+  req = Net::HTTP::Get.new(uri)
+  res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+    http.request(req)
+  }
+  puts res.body
+end
+
+def call_destinations destinations
+  services = destinations.split ","
+  loop do 
+    services.each do |service|
+      begin
+      call_service service
+      rescue => e
+        puts "call service #{service} error: #{e}"
+      end
+    end
+    sleep 1
+  end
+end
+
+if service
+  start_service service
+elsif destinations
+  call_destinations destinations
+else
+  puts "miss env service or destinations, exiting"
+  exit 1
+end
+
+def my_first_private_ipv4
+  ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+  ip && ip.ip_address
+end
+
+# def my_first_public_ipv4
+#   Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_loopback? and !intf.ipv4_multicast? and !intf.ipv4_private?}
+# end
